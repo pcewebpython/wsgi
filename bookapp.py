0679f925a66f5202a4c76b0a4431274a393ed43e
Paul Jurek <pjurek3@gmail.com>
2019-11-22 20:59:19 -0800

    formatted with black

--- bookapp.py
+++ bookapp.py
@@ -4,10 +4,11 @@ from bookdb import BookDB
 
 DB = BookDB()
 
-base_url = 'localhost'
+base_url = "localhost"
 port = 8080
 
 url = f"http://{base_url}:{port}"
+
 
 def book(book_id):
     book_info = DB.title_info(book_id)
@@ -30,20 +31,23 @@ def books():
     response = """<h1>The Great Libary of Nemo</h1>
                   <ul>"""
     for title in DB.titles():
-        response += f"""<li><a href="/books/{title['id']}">{title.get('title')}</a></li>"""
+        response += (
+            f"""<li><a href="/books/{title['id']}">{title.get('title')}</a></li>"""
+        )
 
     response += "</ul>"
-    
+
     return response
+
 
 def resolve_path(path):
     funcs = {
-        '': books,
-        'books': books,
-        'book': book,
+        "": books,
+        "books": books,
+        "book": book,
     }
 
-    path = path.strip('/').split('/')
+    path = path.strip("/").split("/")
 
     func_name = path[0]
     args = path[1:]
@@ -55,36 +59,36 @@ def resolve_path(path):
 
     return func, args
 
+
 def application(environ, start_response):
     status = "200 OK"
-    headers = [('Content-type', 'text/html')]
+    headers = [("Content-type", "text/html")]
     start_response(status, headers)
 
     try:
-        if environ.get('PATH_INFO') == '/':
+        if environ.get("PATH_INFO") == "/":
             response_body = books()
-        
-        elif environ.get('PATH_INFO') == '/books':
+
+        elif environ.get("PATH_INFO") == "/books":
             response_body = books()
-        
-        elif environ.get('PATH_INFO').startswith('/books/id'):
-            input_book_id = environ.get('PATH_INFO').split('/')[2]
+
+        elif environ.get("PATH_INFO").startswith("/books/id"):
+            input_book_id = environ.get("PATH_INFO").split("/")[2]
             response_body = book(input_book_id)
         else:
 
             response_body = f"""<html>
                                 {environ}
                                 </html>
-                                """ 
+                                """
     except NameError:
         response_body = books()
-    
+
+    return [response_body.encode("utf8")]
 
 
-    return [response_body.encode('utf8')]
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     from wsgiref.simple_server import make_server
-    srv = make_server('localhost', 8080, application)
+
+    srv = make_server("localhost", 8080, application)
     srv.serve_forever()
