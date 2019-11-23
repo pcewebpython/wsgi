b7f6f60c13639601c2c073042924a4f632193029
Paul Jurek <pjurek3@gmail.com>
2019-11-22 20:58:48 -0800

    implemented wsgi class exercise

--- bookapp.py
+++ bookapp.py
@@ -4,20 +4,84 @@ from bookdb import BookDB
 
 DB = BookDB()
 
+base_url = 'localhost'
+port = 8080
+
+url = f"http://{base_url}:{port}"
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
+    book_info = DB.title_info(book_id)
+    if book_info is None:
+        raise NameError
+    response = f"""<html>
+                    <h1>{book_info.get('title')}</h1>
+                    <body><p><b>Author:</b> {book_info.get('author')}</p>
+                    <p><b>Publisher:</b> {book_info.get('publisher')}</p>
+                    <p><b>ISBN:</b> {book_info.get('isbn')}</p>
+                    <p><a href="{url}">Back to List</a></p>
+    """
+
+    response += "</body></html>"
+
+    return response
 
 
 def books():
-    return "<h1>a list of books</h1>"
+    response = """<h1>The Great Libary of Nemo</h1>
+                  <ul>"""
+    for title in DB.titles():
+        response += f"""<li><a href="/books/{title['id']}">{title.get('title')}</a></li>"""
 
+    response += "</ul>"
+    
+    return response
+
+def resolve_path(path):
+    funcs = {
+        '': books,
+        'books': books,
+        'book': book,
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+
+    return func, args
 
 def application(environ, start_response):
     status = "200 OK"
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+
+    try:
+        if environ.get('PATH_INFO') == '/':
+            response_body = books()
+        
+        elif environ.get('PATH_INFO') == '/books':
+            response_body = books()
+        
+        elif environ.get('PATH_INFO').startswith('/books/id'):
+            input_book_id = environ.get('PATH_INFO').split('/')[2]
+            response_body = book(input_book_id)
+        else:
+
+            response_body = f"""<html>
+                                {environ}
+                                </html>
+                                """ 
+    except NameError:
+        response_body = books()
+    
+
+
+    return [response_body.encode('utf8')]
 
 
 if __name__ == '__main__':
